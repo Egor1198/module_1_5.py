a219ba5db208d030154e8b4c74491da2effaf619
Egor1198 <belosokhovegor@gmail.com>
2024-08-01 17:57:06 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+# immutable_var = (1, 2, 3, True, 'Яблоко')
+# print(immutable_var)
+
+# immutable_var = (1,2,3, 'Тыква', False)
+# immutable_var [3] = 'Ананас'
+# print(immutable_var)
+# #Кортеж - это не изменяемый список, поэтому его нельзя изменить ((((=
+
+# mutable_list = ['Бармалей','Кощей',500,600,]
+# mutable_list [0:2] = ['Баба Яга', 400, 'Алеша Попович']
+# print(mutable_list)
